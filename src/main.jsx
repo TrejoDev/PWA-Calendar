a5ae4513e52ba0cccba00e243192a2a09d28bb59
Diam <trejodev@gmail.com>
2023-08-12 15:19:16 +0200

    Implemento workbox sw-template

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,25 @@ import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp';
 import './styles.css';
 
+const isProduction = (import.meta.env.MODE === 'production')
+
+// Verificar si el navegador admite Service Workers
+if ( isProduction && 'serviceWorker' in navigator) {
+  // Agregar un evento de carga para registrar el Service Worker
+  window.addEventListener('load', () => {
+    // Registrar el Service Worker
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        // El registro del Service Worker fue exitoso
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch(error => {
+        // Hubo un error al registrar el Service Worker
+        console.error('Error al registrar el Service Worker:', error);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <CalendarApp />
